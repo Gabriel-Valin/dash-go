cbbe15dcbd49870e41972f5cb055cff127262428
Gabriel-Valin <gabrielvalincontato@gmail.com>
2022-01-03 09:05:11 -0300

    feat: responsive Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react"
+import { Flex, Text, Input, Icon, useBreakpointValue} from "@chakra-ui/react"
 import { Logo } from "./logo"
 import { Notification } from "./notification"
 import { Profile } from "./profile"
 import { SearchBox } from "./searchbox"
 
 export const Header = () => {
+    const isLargeScreen = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Flex as='header' w='100%' maxWidth={1480} h='20' mt='4' px='6' mx='auto' align='center'>
             <Logo />
             <SearchBox />
             <Flex align='center' ml='auto'>
                 <Notification />
-                <Profile />
+                <Profile showProfileInfo={isLargeScreen}/>
             </Flex>
         </Flex>
     )
--- src/components/Header/profile.tsx
+++ src/components/Header/profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from "@chakra-ui/react"
 
-export const Profile = () => {
+type ProfileProps = {
+    showProfileInfo?: boolean
+}
+
+export const Profile = ({ showProfileInfo }: ProfileProps) => {
     return (
         <Flex align='center' >
-            <Box mr='4' textAlign='right'>
-                <Text>Gabriel Valin</Text>
-                <Text color='gray.300' fontSize='small'>
-                    gabrielvalin@gmail.com
-                </Text>
-            </Box>
+           { showProfileInfo && (
+                <Box mr='4' textAlign='right'>
+                    <Text>Gabriel Valin</Text>
+                    <Text color='gray.300' fontSize='small'>
+                        gabrielvalin@gmail.com
+                    </Text>
+                </Box>
+           ) }
 
             <Avatar size='md' name='Gabriel Valin' src='https://github.com/gabriel-valin.png' />
         </Flex>
